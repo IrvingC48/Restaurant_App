d76d2acfd780362d3c35b73622764eb6309385ec
IrvingC48 <c.ici.3015@hotmail.com>
2021-10-02 02:01:42 -0500

    modify] declaring env var REACT_APP_ for Netlify

--- src/axios/sendDataForm.js
+++ src/axios/sendDataForm.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const sendDataForm = async (data) => {
-    console.log(process.env.REACT_APP_BASE_URL);
     return axios.post(process.env.REACT_APP_BASE_URL, data)
         .then(response => {
             return response;
